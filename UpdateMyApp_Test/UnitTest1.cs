6876018c5be2d346cbba7b2dc7e456f9d00208a1
m4rcel <m4rcelpl@outlook.com>
2018-02-04 13:07:28 +0100

    New test

--- UpdateMyApp_Test/UnitTest1.cs
+++ UpdateMyApp_Test/UnitTest1.cs
@@ -8,57 +8,60 @@ namespace UpdateMyApp_Test
     [TestClass]
     public class UnitTest1
     {
+        private const string CorrectXmlURL = "https://dl.dropboxusercontent.com/s/mpfyioal1oxbs6v/KeyHolderUpdate.xml";
+        private const string BadXmlURL = "https://dl.dDropboxusercontent.com/s/mpfyioal1oxbs6v/KeyHolderUpdate.xml";
+
         [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
         public async Task CheckForNewVersionBadDataAsync()
         {
             Update.IsEnableError = true;
 
             try
-            {
+            {              
                 await Update.CheckForNewVersionAsync(null, null);
-                Assert.Fail("Expect exception [1]");
+                //Assert.Fail("Expect exception [1]");
 
-                await Update.CheckForNewVersionAsync("https://dl.dropboxusercontent.com/s/mpfyioal1oxbs6v/KeyHolderUpdate.xml", null);
-                Assert.Fail("Expect exception [2]");
+                await Update.CheckForNewVersionAsync(CorrectXmlURL, null);
+                //Assert.Fail("Expect exception [2]");
 
                 await Update.CheckForNewVersionAsync(null, "1.4.4");
-                Assert.Fail("Expect exception [3]");
+                //Assert.Fail("Expect exception [3]");
 
                 await Update.CheckForNewVersionAsync("", "");
-                Assert.Fail("Expect exception [4]");
+                //Assert.Fail("Expect exception [4]");
 
-                await Update.CheckForNewVersionAsync("https://dl.dropboxusercontent.com/s/mpfyioal1oxbs6v/KeyHolderUpdate.xml", "");
-                Assert.Fail("Expect exception [5]");
+                await Update.CheckForNewVersionAsync(CorrectXmlURL, "");
+                //Assert.Fail("Expect exception [5]");
 
                 await Update.CheckForNewVersionAsync("", "1.4.4");
-                Assert.Fail("Expect exception [6]");
+                //Assert.Fail("Expect exception [6]");
 
                 await Update.CheckForNewVersionAsync(" ", " ");
-                Assert.Fail("Expect exception [7]");
+                //Assert.Fail("Expect exception [7]");
 
-                await Update.CheckForNewVersionAsync("https://dl.dropboxusercontent.com/s/mpfyioal1oxbs6v/KeyHolderUpdate.xml", " ");
-                Assert.Fail("Expect exception [8]");
+                await Update.CheckForNewVersionAsync(CorrectXmlURL, " ");
+                //Assert.Fail("Expect exception [8]");
 
                 await Update.CheckForNewVersionAsync(" ", "1.4.4");
-                Assert.Fail("Expect exception [9]");
+                //Assert.Fail("Expect exception [9]");
             }
             catch (Exception)
             {
-            }
-            
+                Assert.Fail("Expect exception [10]");
+            }            
         }
 
         [TestMethod]
         public async Task CheckForNewVersionTrueAsync()
         {
-            Assert.IsTrue(await Update.CheckForNewVersionAsync("https://dl.dropboxusercontent.com/s/mpfyioal1oxbs6v/KeyHolderUpdate.xml", "1.4.0"));
+            Assert.IsTrue(await Update.CheckForNewVersionAsync(CorrectXmlURL, "1.4.0"));
         }
 
         [TestMethod]
         public async Task CheckForNewVersionFalseAsync()
         {
-            Assert.IsFalse(await Update.CheckForNewVersionAsync("https://dl.dropboxusercontent.com/s/mpfyioal1oxbs6v/KeyHolderUpdate.xml", "1.4.5"));
+            Assert.IsFalse(await Update.CheckForNewVersionAsync(CorrectXmlURL, "1.4.5"));
         }
-
     }
 }
